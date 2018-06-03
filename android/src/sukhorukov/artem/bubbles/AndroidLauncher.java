b3c227e1185f35c2f7dcb0f91fd000034c62522a
Sukhorukov Artem <soohoy@bk.ru>
2018-06-03 11:03:06 +0500

    first commit

---
+++ android/src/sukhorukov/artem/bubbles/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package sukhorukov.artem.bubbles;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import sukhorukov.artem.bubbles.Bubbles;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Bubbles(), config);
+	}
+}
---
+++ desktop/src/sukhorukov/artem/bubbles/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package sukhorukov.artem.bubbles.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import sukhorukov.artem.bubbles.Bubbles;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Bubbles(), config);
+	}
+}
